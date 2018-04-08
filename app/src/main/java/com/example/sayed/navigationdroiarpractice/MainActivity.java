4a47233c888f0574dd3b2e670908f39b5eb68c6e
sayed <nuruddinsayeed@gmail.com>
2018-04-08 14:15:03 +0600

    ToolbarAdded

--- app/src/main/java/com/example/sayed/navigationdroiarpractice/MainActivity.java
+++ app/src/main/java/com/example/sayed/navigationdroiarpractice/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,20 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        toolbar = findViewById(R.id.toolbarid);
+        toolbar = findViewById(R.id.toolbarID);
         setSupportActionBar(toolbar);
     }
 
-/*    @Override
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
-
+        getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.itemOne){
+            Toast.makeText(this, "Item One Selected", Toast.LENGTH_SHORT).show();
+        }else if (id == R.id.itemTwo){
+            Toast.makeText(this, "Item Two Selected", Toast.LENGTH_SHORT).show();
+        }else if (id == R.id.itemThree){
+            Toast.makeText(this, "Item Three Selected", Toast.LENGTH_SHORT).show();
+        }
+
         return super.onOptionsItemSelected(item);
-    }*/
+    }
 }
